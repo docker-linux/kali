f593a67c91d97326fb5bede42d41024551acd484
Tom Eklöf <tom.eklof@rg19.se>
2014-08-14 08:21:09 +0200

    added debootstrap dependency. Added sudo for rm cmnd

--- build-kali.sh
+++ build-kali.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Install dependencies (debbootstrap)
+sudo apt-get install debootstrap
+
 # Fetch the latest Kali debootstrap script from git
 curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=HEAD" > kali-debootstrap &&\
 sudo debootstrap kali ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
 sudo tar -C kali-root -c . | sudo docker import - linux/kali &&\
-rm -rf ./kali-root &&\
+sudo rm -rf ./kali-root &&\
 docker run -t -i linux/kali cat /etc/debian_version &&\
 echo "Build OK" || echo "Build failed!"
