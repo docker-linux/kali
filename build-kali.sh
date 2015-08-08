812b6b01b0d5f30587a92c7702110bdc5391aa40
silverfisk <tom@linux-konsult.com>
2015-08-08 11:46:44 +0200

    changed username from linux to linuxkonsult. Corrected url for debootstrap

--- build-kali.sh
+++ build-kali.sh
@@ -4,9 +4,9 @@
 sudo apt-get install debootstrap
 
 # Fetch the latest Kali debootstrap script from git
-curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=HEAD" > kali-debootstrap &&\
+curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=refs/heads/kali/master" > kali-debootstrap &&\
 sudo debootstrap kali ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
-sudo tar -C kali-root -c . | sudo docker import - linux/kali &&\
+sudo tar -C kali-root -c . | sudo docker import - linuxkonsult/kali &&\
 sudo rm -rf ./kali-root &&\
-docker run -t -i linux/kali cat /etc/debian_version &&\
+sudo docker run -t -i linuxkonsult/kali cat /etc/debian_version &&\
 echo "Build OK" || echo "Build failed!"
