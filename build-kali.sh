85f68b8780c5899aa95c72190d6e8e1a04104c35
silverfisk <tom@linux-konsult.com>
2015-08-13 06:13:33 +0200

    Tagging with version

--- build-kali.sh
+++ build-kali.sh
@@ -6,7 +6,9 @@ sudo apt-get install debootstrap
 # Fetch the latest Kali debootstrap script from git
 curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=refs/heads/kali/master" > kali-debootstrap &&\
 sudo debootstrap kali ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
-sudo tar -C kali-root -c . | sudo docker import - linuxkonsult/kali &&\
+sudo tar -C kali-root -c . | sudo docker import - linuxkonsult/kali:latest &&\
 sudo rm -rf ./kali-root &&\
-sudo docker run -t -i linuxkonsult/kali cat /etc/debian_version &&\
+TAG=$(sudo docker run -t -i linuxkonsult/kali awk '{print $NF}' /etc/debian_version | sed 's/\r$//' ) &&\
+echo "Tagging kali with $TAG" &&\
+sudo docker tag linuxkonsult/kali:latest linuxkonsult/kali:$TAG &&\
 echo "Build OK" || echo "Build failed!"
