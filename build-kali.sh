ae7133e49be96e23367f77de4ca6028faa570811
Tom Eklöf <tom.eklof@scania.com>
2017-08-31 21:06:22 +0200

    Fixes #2

--- build-kali.sh
+++ build-kali.sh
@@ -5,7 +5,7 @@ sudo apt-get install debootstrap
 
 # Fetch the latest Kali debootstrap script from git
 curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=refs/heads/kali/master" > kali-debootstrap &&\
-sudo debootstrap kali-current ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
+sudo debootstrap kali-rolling ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
 sudo tar -C kali-root -c . | sudo docker import - linuxkonsult/kali:latest &&\
 sudo rm -rf ./kali-root &&\
 TAG=$(sudo docker run -t -i linuxkonsult/kali awk '{print $NF}' /etc/debian_version | sed 's/\r$//' ) &&\
